109eb74aca3cf1cea5fb840f46a742272de7dc4d
adave2904 <aarsh.dave13@gmail.com>
2020-01-23 04:45:58 -0500

    Create Exercise1.R
    Write a R program to take input from the user (name and age) and display the values. Also print the version of R installation.

---
+++ w3resources/Exercise1.R
@@ -0,0 +1,7 @@
+name <- readline(prompt = "Enter the name: ")
+age <- readline(prompt = "Enter age: ")
+
+print(paste("Name: ",name))
+print(paste("Age: ",age))
+
+print(paste("R version: ",R.version.string))
