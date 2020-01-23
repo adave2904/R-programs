bd8f0cfcbd360bb80e04f9bdff0b042ccccaf76b
adave2904 <aarsh.dave13@gmail.com>
2020-01-23 04:45:24 -0500

    Delete Name_age_version_print.R

--- w3resources/Name_age_version_print.R
+++
@@ -1,7 +0,0 @@
-name <- readline(prompt = "Enter the name: ")
-age <- readline(prompt = "Enter age: ")
-
-print(paste("Name: ",name))
-print(paste("Age: ",age))
-
-print(paste("R version: ",R.version.string))
