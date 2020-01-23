3ea56a94145920e31e7ff25118eb9f063b3f6853
adave2904 <aarsh.dave13@gmail.com>
2020-01-23 04:44:49 -0500

    Create Exercise2.R
    Write a R program to create a sequence of numbers from 20 to 50 and find the mean of numbers from 20 to 60 and sum of numbers from 51 to 91.

---
+++ w3resources/Exercise2.R
@@ -0,0 +1,10 @@
+#Create a sequence of numbers from 20 to 50
+v1 <- seq(20,50)
+print("Sequence of numbers from 20 to 50: ")
+print(v1)
+
+#Find the mean of numbers from 20 to 60
+print(paste("The mean of numbers from 20 to 60: ",mean(20:60)))
+
+#Sum of numbers from 51 to 91.
+print(paste("The sum of numbers from 51 to 91: ",sum(51:91)))
