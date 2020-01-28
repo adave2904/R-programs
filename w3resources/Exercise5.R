9a2315c0c3c91ec8d114332d82021d0dd09db1e9
adave2904 <aarsh.dave13@gmail.com>
2020-01-28 07:27:47 -0500

    Create Exercise5.R
    Take a vector and print the max and min element of the vector.

---
+++ w3resources/Exercise5.R
@@ -0,0 +1,5 @@
+#Take a vector and print the max and min element of the vector.
+v <- c(10,20,30,40,50,60)
+
+print(paste("Maximum element of the vector: ",max(v)))
+print(paste("Minimum element of the vector: ",min(v)))
