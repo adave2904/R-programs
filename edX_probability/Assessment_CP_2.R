906308a06985a9f0e94796907e313f392c47018b
adave2904 <aarsh.dave13@gmail.com>
2020-04-15 17:53:02 +0530

    Delete Assessment_CP_2.R

--- edX_probability/Assessment_CP_2.R
+++
@@ -1,9 +0,0 @@
-# Create a vector with a sequence of integers from 1 to 36.
-x <- seq(1,36)
-
-# Use dnorm to determine the value of the probability density function over x given a mean of 20.9
-# and standard deviation of 5.7; save the result as f_x.
-f_x <- dnorm(x,20.9,5.7)
-
-# Plot x against f_x.
-plot(x,f_x)
